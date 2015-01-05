5c1eaa18d6d38c69f831488af3f9ae8c5dc51919
JasonLy <jason.ly@gmail.com>
2015-01-05 14:03:04 -0800

    Test passes for getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var answer = [];
+
+  var traverse = function(start){
+  	for(var i = 0; i < start.children.length; i++){
+  		if(start.children[i].classList.contains(className)){
+  			answer.push(start.children[i]);
+  		}
+  		if(start.children[i].hasChildNodes()){
+  			traverse(start.children[i]);
+  		}
+  	}
+  }
+  
+  traverse(document);
+  return answer;
 };
+
+
+// notes:
+// somethingGeneric.children[i].className will return the classname could probably compare but the function's
+// parameter already uses className. Tried to change it but it doesn't work with the test
+// if(start.children[i].className === classNameBeingCalled)
+// the recursive part probably happens when there are nested elements, so if an element has more children
+// then call traverse again on that element.
